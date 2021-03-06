219d0b103b88674698b5edc3dc42ffa7ae8bdcb7
Spacener <spst7341@colorado.edu>
2021-03-06 13:12:44 -0700

    Geolocation data gathered on home.html

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const run = app.listen(PORT, () => {
     console.log("server is running!");
 })
 
-app.get('/home', (req, res,next)=>{
-    res.send(home.html);
+app.get('/home', function(req, res) {
+    res.sendFile('views/home.html', {root: __dirname })
 });
 
