59603aa7e47efd08a30552775233c8c90e2f1dde
eurekesh <asdu8905@colorado.edu>
2021-03-06 10:59:42 -0700

    second

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function(req,res){
+    res.send('hello');
+})
+
+const run = app.listen(PORT, () => {
+    console.log("server is running!");
+})
