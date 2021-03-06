de0e9b83bea890244098e8a19f643866f337f860
eurekesh <asdu8905@colorado.edu>
2021-03-06 11:33:39 -0700

    third

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
+var unirest = require("unirest");
 
 app.get('/', function(req,res){
-    res.send('hello');
+    var reqa = unirest("GET", "https://uphere-space1.p.rapidapi.com/user/visible");
+
+    reqa.query({
+        "lng": "122.374199",
+        "lat": "47.6484346"
+    });
+
+    reqa.headers({
+        "x-rapidapi-key": "8ffab72fe1msh1d8c4b5b79c829bp1f9ae7jsn89b1caa76282",
+        "x-rapidapi-host": "uphere-space1.p.rapidapi.com",
+        "useQueryString": true
+    });
+
+    let response;
+    reqa.end(function (resa) {
+        if (res.error) throw new Error(resa.error);
+        response = resa.body;
+        console.log(response);
+    })
+
 })
 
 const run = app.listen(PORT, () => {
     console.log("server is running!");
 })
+
+
+
