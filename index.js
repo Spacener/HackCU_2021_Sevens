f88e288febc649db494a64921260bc203ac7d733
georgej1144 <georgejnsn1144@gmail.com>
2021-03-06 15:01:12 -0700

    socketio sort of working but not really

--- index.js
+++ index.js
@@ -1,40 +1,67 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const path = require('path')
+const app = express();
 const PORT = process.env.PORT || 3000;
-var unirest = require("unirest");
-
+const unirest = require("unirest");
+const http = require('http').createServer(app);
+const io = require('socket.io')(http)
 
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
 }
 
+app.use(express.static(path.join(__dirname,"views")))
+
+app.get('/home', function(req, res) {
+    res.sendFile('views/home.html', {root: __dirname })
+});
+
 app.get('/', function(req,res){
-    let observer_lat, observer_lng, observer_alt, search_radius, category_id;
+    res.sendFile('views/home.html', {root: __dirname })
+})
+app.get('/socket.io/socket.io.js', (req,res)=>{
+    res.sendFile('node_modules/socket.io/client-dist/socket.io.js',{root: __dirname });
+})
+app.get('/test', function(req,res){
+    res.redirect('p5Test.html')
+})
 
-    observer_lat = 41.702;
-    observer_lng = -76.014;
-    observer_alt = 0;
-    search_radius = 70;
-    category_id = 0;
+io.on('connection', function(socket) {
+    console.log("client connected! client id: " + socket.id);
+    socket.on('data requested', function() {
+        console.log("data has been requested")
+        let observer_lat, observer_lng, observer_alt, search_radius, category_id;
 
-    var reqa = unirest("GET", "https://api.n2yo.com/rest/v1/satellite/above/" +
-        observer_lat +
-        "/" +
-        observer_lng +
-        "/" +
-        observer_alt +
-        "/" +
-        search_radius +
-        "/" +
-        category_id +
-        "/&apiKey=" +
-        process.env.SAT_KEY);
+        observer_lat = 41.702;
+        observer_lng = -76.014;
+        observer_alt = 0;
+        search_radius = 70;
+        category_id = 0;
 
-    let response;
-    reqa.end(function (resa) {
-        if (res.error) throw new Error(resa.error);
-        response = resa.body;
-        res.send(response);
+        var reqa = unirest("GET", "https://api.n2yo.com/rest/v1/satellite/above/" +
+            observer_lat +
+            "/" +
+            observer_lng +
+            "/" +
+            observer_alt +
+            "/" +
+            search_radius +
+            "/" +
+            category_id +
+            "/&apiKey=" +
+            process.env.SAT_KEY);
+
+        let response;
+        reqa.end(function (resa) {
+            if (res.error) throw new Error(resa.error);
+            response = resa.body;
+
+            // const ResponseAsJson = JSON.parse(response)
+
+            console.log("Length:" + response.info.satcount);
+            let coordsToRespondWith = [[5,5],[10,10]];
+            socket.emit('satellite data array', coordsToRespondWith);
+        })
     })
 })
 
@@ -42,9 +69,7 @@ const run = app.listen(PORT, () => {
     console.log("server is running!");
 })
 
-app.get('/home', function(req, res) {
-    res.sendFile('views/home.html', {root: __dirname })
-});
+
 
 app.get('/aboutus', function(req, res) {
     res.sendFile('views/aboutus.html', {root: __dirname })
