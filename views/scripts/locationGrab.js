b00e1a12f125c94cccbaf4c00c983e1793db9cf0
georgej1144 <georgejnsn1144@gmail.com>
2021-03-07 01:37:58 -0700

    fine touches on scatter and heatmap

--- views/scripts/locationGrab.js
+++ views/scripts/locationGrab.js
@@ -1,16 +1,20 @@
+var userLat = 0
+var userLong = 0;
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     }
-    if(userLong !== 0 && userLat !== 0) {
-        socket.emit('locationSent', [userLong, userLat]);
-    }
 }
 
-var userLat, userLong;
+
 
 function showPosition(position) {
     userLat = position.coords.latitude;
     userLong = position.coords.longitude;
     document.getElementById('dataButton').disabled = false;
+    if(userLong !== 0 && userLat !== 0) {
+        // console.log(userLat + " " + userLong + "543534")
+        socket.emit('locationSent', [userLong, userLat]);
+    }
 }
--- views/scripts/plot.js
+++ views/scripts/plot.js
@@ -2,47 +2,28 @@ var socket = io();
 
 var satData = {};
 
-var data = {
-    type: "scatter3d",
-    x: [],
-    y: [],
-    z: [],
-    text: [],
-    mode: 'markers',
-    hoverinfo: "text",
-    marker: {
-        symbol: 'circle',
-        color: 'rgb(127, 127, 127)',
-        size: 6,
-        line: {
-            color: 'rgba(217, 217, 217, 0.14)',
-            width: 0.5
-        },
-        opacity: 0.8
-    }
-}
 
-var data1 = {
-    type: 'densitymapbox',
-    lon: [],
-    lat: [],
-    z: [],
-    text: [],
-    hoverinfo: "text"
-};
+
+var scatterSatData;
+
+var scatterUserLocation;
+
+var dmapboxSatData;
+
+var dmapboxUserLocation;
 
 function formArrayFromObj(inData) {
     for(let i in inData) {
-        if(inData[i][2] < 4000000) {
-            data['x'].push(inData[i][0]);
-            data['y'].push(inData[i][1]);
-            data['z'].push(inData[i][2]);
-            data['text'].push(inData[i][3]);
-            data1['text'].push(inData[i][3]);
+        if(inData[i][2] < 100000000000) {
+            scatterSatData['x'].push(inData[i][0]);
+            scatterSatData['y'].push(inData[i][1]);
+            scatterSatData['z'].push(inData[i][2]);
+            scatterSatData['text'].push(inData[i][3]);
+            dmapboxSatData['text'].push(inData[i][3]);
         }
-        data1['lon'] = data['y'];
-        data1['lat'] = data['x'];
-        data1['z'] = 1;
+        dmapboxSatData['lon'] = scatterSatData['y'];
+        dmapboxSatData['lat'] = scatterSatData['x'];
+        dmapboxSatData['z'] = 1;
     }
 }
 
@@ -52,35 +33,111 @@ var layout = {
         r: 0,
         b: 0,
         t: 0,
+    },
+    scene: {
         zaxis: {
             type: 'log',
-            autorange: true
+            autorange: true,
+            title: 'Kilometers from surface'
+        },
+        xaxis: {
+            title: 'Longitude'
+        },
+        yaxis: {
+            title: 'Latitude'
         }
     }
+
 };
 
 
 
 var layout1 = {
-    radius: 1,
     mapbox: {
         style: 'stamen-terrain'
     }
 };
 
+function resetAll() {
+    scatterSatData = {
+        type: "scatter3d",
+        text: [],
+        x: [],
+        y: [],
+        z: [],
+        mode: 'markers',
+        hoverinfo: "text",
+        //surfacecolor: 'rgb(126,201,216)',
+        marker: {
+            symbol: 'circle',
+            size: 3,
+            line: {
+                color: 'rgba(40, 40, 40, 0.14)',
+                width: 0.5
+            },
+            opacity: 0.8
+        }
+    }
 
+    scatterUserLocation = {
+        type: "scatter3d",
+        x: [],
+        y: [],
+        z: [1],
+        text: ['Your location', 'test'],
+        mode: 'markers',
+        hoverinfo: "text",
+        surfacecolor: 'rgb(255,0,0)',
+        marker: {
+            symbol: 'circle',
+            size: 3,
+            line: {
+                color: 'rgba(40, 40, 40, 0.14)',
+                width: 0.5
+            },
+            opacity: 0.8
+        }
+    }
+
+    dmapboxSatData = {
+        type: 'densitymapbox',
+        lon: [],
+        lat: [],
+        text: [],
+        hoverinfo: 'lat+lon',
+        radius: 15,
+        autocolorscale: false,
+        // colorscale: [[0, 'rgba(0,255,0,5)'], [1,'rgb(126,201,216)']],
+        showscale: false
+    };
+
+    dmapboxUserLocation = {
+        type: 'densitymapbox',
+        lon: [],
+        lat: [],
+        name: "Your Location",
+        hoverinfo: "name",
+        radius: 30,
+        showscale: false
+        //colorscale: [[0, 'rgb(0,255,0)'], [1, 'rgb(0,255,0)']]
+    }
+}
 
 function drawPlot() {
     var myPlot = document.getElementById('graph');
-    Plotly.newPlot('graph', [data], layout)
-    Plotly.newPlot('heatmap', [data1], layout1);
+    // console.log([ourLocation])
+    Plotly.newPlot('graph', [scatterSatData, scatterUserLocation], layout)
+    Plotly.newPlot('heatmap', [dmapboxSatData, dmapboxUserLocation], layout1);
 }
 
-
 socket.on('satellite data array', function(data){
-    // whatever
-    satData = data;
-    console.log("gotData");
+    resetAll();
+    // console.log(userLat + " " + userLong)
+    scatterUserLocation['x'].push(userLat);
+    scatterUserLocation['y'].push(userLong);
+    dmapboxUserLocation['lat'].push(userLat);
+    dmapboxUserLocation['lon'].push(userLong);
+
     formArrayFromObj(data);
     drawPlot();
 })
