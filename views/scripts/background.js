b853e0d751a1883bb3c41506e3086262b791f7c7
Spacener <spst7341@colorado.edu>
2021-03-06 18:21:10 -0700

    Home page changes

--- views/scripts/background.js
+++ views/scripts/background.js
@@ -2,10 +2,10 @@ particlesJS("particles-js",
     {
         "particles": {
             "number": {
-                "value": 160,
+                "value": 100,
                 "density": {
                     "enable": true,
-                    "value_area": 800
+                    "value_area": 1657
                 }
             },
             "color": {
@@ -19,15 +19,20 @@ particlesJS("particles-js",
                 },
                 "polygon": {
                     "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
                 }
             },
             "opacity": {
-                "value": 1,
-                "random": true,
+                "value": 0.5,
+                "random": false,
                 "anim": {
-                    "enable": true,
+                    "enable": false,
                     "speed": 1,
-                    "opacity_min": 0,
+                    "opacity_min": 0.1,
                     "sync": false
                 }
             },
@@ -36,13 +41,13 @@ particlesJS("particles-js",
                 "random": true,
                 "anim": {
                     "enable": false,
-                    "speed": 4,
-                    "size_min": 0.3,
+                    "speed": 40,
+                    "size_min": 0.1,
                     "sync": false
                 }
             },
             "line_linked": {
-                "enable": false,
+                "enable": true,
                 "distance": 150,
                 "color": "#ffffff",
                 "opacity": 0.4,
@@ -50,16 +55,16 @@ particlesJS("particles-js",
             },
             "move": {
                 "enable": true,
-                "speed": 1,
+                "speed": 0.4,
                 "direction": "none",
-                "random": true,
+                "random": false,
                 "straight": false,
                 "out_mode": "out",
                 "bounce": false,
                 "attract": {
                     "enable": false,
                     "rotateX": 600,
-                    "rotateY": 600
+                    "rotateY": 1200
                 }
             }
         },
@@ -68,30 +73,30 @@ particlesJS("particles-js",
             "events": {
                 "onhover": {
                     "enable": true,
-                    "mode": "bubble"
+                    "mode": "grab"
                 },
                 "onclick": {
-                    "enable": true,
-                    "mode": "repulse"
+                    "enable": false,
+                    "mode": "push"
                 },
                 "resize": true
             },
             "modes": {
                 "grab": {
-                    "distance": 400,
+                    "distance": 200,
                     "line_linked": {
                         "opacity": 1
                     }
                 },
                 "bubble": {
-                    "distance": 250,
-                    "size": 0,
+                    "distance": 400,
+                    "size": 40,
                     "duration": 2,
-                    "opacity": 0,
+                    "opacity": 8,
                     "speed": 3
                 },
                 "repulse": {
-                    "distance": 400,
+                    "distance": 200,
                     "duration": 0.4
                 },
                 "push": {
@@ -103,4 +108,5 @@ particlesJS("particles-js",
             }
         },
         "retina_detect": true
-    })
+    }
+    )
